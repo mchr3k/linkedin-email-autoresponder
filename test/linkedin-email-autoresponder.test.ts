b951f0934e107c9e6e818da7f92c6137d9aadacc
Martin Hare Robertson 🚀🌈 <martin.hare_robertson@bp.com>
2023-03-18 20:44:07 +0000

    Initial commit

---
+++ test/linkedin-email-autoresponder.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LinkedinEmailAutoresponder from '../lib/linkedin-email-autoresponder-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/linkedin-email-autoresponder-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LinkedinEmailAutoresponder.LinkedinEmailAutoresponderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
