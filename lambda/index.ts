5d9ebf942560536467df0eb2d11fd7476d4d2753
Martin Hare Robertson 🚀🌈 <martin.hare_robertson@bp.com>
2023-03-19 00:08:04 +0000

    Connect up to API Gateway

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,4 +1,8 @@
-export const handler = async (event: unknown): Promise<unknown> => {
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<unknown> => {
   console.log('Event:', JSON.stringify(event, null, 2));
   return { statusCode: 200, body: 'Hello from Lambda!' };
 };
--- lib/linkedin-email-autoresponder-stack.ts
+++ lib/linkedin-email-autoresponder-stack.ts
@@ -1,17 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
+import * as apigateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import * as integrations from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LinkedinEmailAutoresponderStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'LinkedinEmailAutoresponderHandler', {
-      functionName: 'LinkedinEmailAutoresponderHandler',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('dist/lambda'),
-      handler: 'index.handler',
+    const lambdaFunction = new lambda.Function(
+      this,
+      'LinkedinEmailAutoresponderHandler',
+      {
+        functionName: 'LinkedinEmailAutoresponderHandler',
+        runtime: lambda.Runtime.NODEJS_18_X,
+        code: lambda.Code.fromAsset('dist/lambda'),
+        handler: 'index.handler',
+      }
+    );
+
+    const httpApi = new apigateway.HttpApi(this, 'api-gateway', {
+      corsPreflight: {
+        allowHeaders: ['Content-Type'],
+        allowMethods: [
+          apigateway.CorsHttpMethod.GET,
+          apigateway.CorsHttpMethod.POST,
+          apigateway.CorsHttpMethod.OPTIONS,
+        ],
+        allowOrigins: ['*'],
+      },
+    });
+
+    httpApi.addRoutes({
+      path: '/{proxy+}',
+      methods: [apigateway.HttpMethod.ANY],
+      integration: new integrations.HttpLambdaIntegration(
+        'http-lambda',
+        lambdaFunction
+      ),
+    });
+
+    new cdk.CfnOutput(this, 'HttpApiUrl', {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      value: httpApi.url!,
     });
   }
 }
